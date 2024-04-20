ff6056cb2733d73cacb71a0bccbcbc3566ab0c29
Juan Orduz <juanorpinilla@gmail.com>
2024-04-20 14:49:46 -0500

    .joblib

--- backend/app/Resultados.py
+++
@@ -1,8 +0,0 @@
-import pipeline
-import pandas as pd
-
-# Load the data
-df = pd.read_csv('/content/tipo2_entrenamiento_estudiantes.csv')
-
-# Fit the pipeline
-X_train, X_test, Y_train, Y_test = pipeline.fit_transform(df)
--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,23 @@
+from backend.app import DataModel
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import pandas as pd
+from joblib import load
+from sklearn.metrics import classification_report
+
+import numpy as np
+import string
+import unicodedata
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+
 
 class Review:
     def __init__(self, review_id, text):
@@ -30,6 +47,22 @@ async def upload_file(file: UploadFile = File(...)):
         df = pd.read_csv(file.file)
         
         file.file.close()
+
+
+        # Fit the pipeline
+        train, test = train_test_split(df, test_size=0.2, random_state=9)
+
+
+        #estos es los resultados del modelo
+        x_test = test.drop(['Class'],axis=1)
+        y_test = test['Class']
+
+        model = load("assets/modelo.joblib")
+        
+        y_pred = model.predict(x_test)
+
+        print(classification_report(y_test, y_pred))
+
         for index, row in df.iterrows():
             review = Review(review_id=index+1, text=row["Review"])
             reviews.append(review.to_dict())
@@ -37,3 +70,4 @@ async def upload_file(file: UploadFile = File(...)):
 
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
+    
